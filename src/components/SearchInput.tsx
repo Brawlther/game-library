736ef67320470e5ef8f84c0c171856a393b4d774
ZhiruXie <danielxie9735@gmail.com>
2025-05-21 22:59:36 -0400

    Implement searching

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -4,18 +4,36 @@ import {
   InputLeftElement,
   useBreakpointValue,
 } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
   const placeholder = useBreakpointValue({
     base: "Search",
     sm: "Search Games...", // up to 480px
   });
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />}></InputLeftElement>
-      <Input borderRadius={20} placeholder={placeholder} variant="filled" />
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />}></InputLeftElement>
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder={placeholder}
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
