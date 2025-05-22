49b7a8be6a504c3d7576f34e6b16df25c989a981
ZhiruXie <danielxie9735@gmail.com>
2025-05-21 23:34:34 -0400

    Adding emoji for game top rating

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { type Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -21,9 +22,10 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize={"md"} textAlign={"center"} paddingBottom={"10px"}>
+        <Heading fontSize={"md"} textAlign="center">
           {game.name}
         </Heading>
+        <Emoji rating={game.rating_top} />
       </CardBody>
     </Card>
   );
